fb44934277c5500df533571b8ec925a75498c109
Gabriele Balletti <gabriele.balletti@gmail.com>
2023-08-08 22:39:56 +0200

    Ignore unused imports in init file

--- src/sympol/__init__.py
+++ src/sympol/__init__.py
@@ -1,3 +1,3 @@
-from sympol.point import Point
-from sympol.point_configuration import PointConfiguration
-from sympol.polytope import Polytope
+from sympol.point import Point  # noqa: F401
+from sympol.point_configuration import PointConfiguration  # noqa: F401
+from sympol.polytope import Polytope  # noqa: F401
