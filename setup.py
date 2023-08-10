d660a9bae36f3ea1a04d9bbb4445d6a49174b90f
Gabriele Balletti <gabriele.balletti@gmail.com>
2023-08-10 15:09:25 +0200

    Move install_requires to setup.py (#5)
    * Move install_requires to setup.py
    
    * Add numpy as a build-system dependency
    
    * Add numpy as a build-system dependency
    
    * Remove obsolete requirements.txt
    
    * Revert to standard pip installation in doc pipeline
    
    * Revert to standard pip installation in doc pipeline
    
    * Revert to standard pip installation in doc pipeline
    
    * Revert to standard pip installation in doc pipeline
    
    * Revert to standard pip installation in doc pipeline
    
    * Move all deps to setup.py
    
    * Move all deps to setup.py

--- setup.py
+++ setup.py
@@ -5,4 +5,23 @@ from numpy import get_include as numpy_get_include
 setup(
     ext_modules=cythonize(["./src/sympol/*.pyx"]),
     include_dirs=[numpy_get_include()],
+    install_requires=[
+        "igraph>=0.10.4",
+        "numpy>=1.24.3",
+        "pycddlib>=2.1.6",
+        "scipy>=0.12.0",
+        "sympy>=1.12",
+    ],
+    extras_require={
+        "dev": [
+            "black",
+            "pytest",
+            "pytest-cov",
+            "ruff",
+        ],
+        "docs": [
+            "myst-parser",
+            "sphinx-rtd-theme",
+        ],
+    },
 )
