e82ad016dc02dfb14d5a974b8ab8c2a55495fd83
Gabriele Balletti <gabriele.balletti@gmail.com>
2023-08-10 17:37:07 +0200

    Allow rtd build without editable installation

--- docs/conf.py
+++ docs/conf.py
@@ -3,7 +3,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("../src"))
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
