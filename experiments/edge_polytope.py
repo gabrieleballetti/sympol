e0227d36c756bbd40f741e99c93b56cf26c6b69c
Gabriele Balletti <gabriele.balletti@gmail.com>
2023-08-16 18:29:56 +0200

    Add edge polytope script (#9)

---
+++ experiments/edge_polytope.py
@@ -0,0 +1,24 @@
+import numpy as np
+from sympol import Polytope
+from igraph import Graph
+
+
+def random_edge_polytope(n_vertices, edge_prob):
+    graph = Graph.Erdos_Renyi(n_vertices, edge_prob)
+    edges = graph.get_edgelist()
+    verts = np.zeros((len(edges), n_vertices), dtype=int)
+    for i, (u, v) in enumerate(edges):
+        verts[i, u] = 1
+        verts[i, v] = 1
+    return Polytope(vertices=verts[:, 1:])
+
+
+if __name__ == "__main__":
+    dim = 13
+    while True:
+        p = random_edge_polytope(n_vertices=dim, edge_prob=0.5)
+        if p.dim < dim - 1:
+            continue
+        print(p.h_star_vector)
+        if not p.is_ehrhart_positive:
+            print(p.vertices)
