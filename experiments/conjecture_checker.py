636d265908074f5aa75dd5c8576d67726ff0fdbf
Gabriele Balletti <gabriele.balletti@gmail.com>
2023-08-19 01:00:39 +0200

    Add conjecture_checker experiment

---
+++ experiments/conjecture_checker.py
@@ -0,0 +1,38 @@
+import numpy as np
+from sympol import Polytope
+
+
+def sample_polytope_from_normal_distribution(dim, n_points, std_dev=1):
+    """
+    Return a random polytope as the convex hull of a set of points sampled from a
+    normal distribution.
+    """
+    if dim < 1:
+        raise ValueError("Dimension must be at least 1.")
+    if n_points < dim + 1:
+        raise ValueError("Number of points must be at least dim + 1.")
+
+    d = -1
+    while d != dim:
+        verts = np.random.default_rng().normal(
+            loc=0, scale=std_dev, size=(n_points, dim)
+        )
+        verts = verts.round().astype(int)
+        p = Polytope(verts)
+        d = p.dim
+    return Polytope(verts)
+
+
+if __name__ == "__main__":
+    dim = 6
+    extra_pts = 1
+    while True:
+        p = sample_polytope_from_normal_distribution(dim, n_points=dim + 1 + extra_pts)
+        if p.dim < dim:
+            continue
+
+        if not p.is_idp:
+            continue
+
+        if not p.has_log_concave_h_star_vector:
+            print(p.vertices)
