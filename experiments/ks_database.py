c5e4b3fad559cd218473e95e4a7f3413a8bdf688
Gabriele Balletti <gabriele.balletti@gmail.com>
2023-10-03 20:09:37 +0200

    Fix ruff warnings

--- experiments/ks_database.py
+++ experiments/ks_database.py
@@ -2,7 +2,6 @@ import numpy as np
 from sympol import Polytope
 import pathlib
 from itertools import islice
-from sympol._utils import _arrays_equal_up_to_row_permutation
 
 DATA_FOLDER = ".data/4p_reflexive"
 
@@ -38,36 +37,15 @@ if __name__ == "__main__":
             continue
         print(f"Checking v{n_verts:02d}...")
 
-        # if n_verts % 2 != 0:
-        #     n_verts += 1
-        #     continue
-
         for lines in _get_n_lines_iterator(filename):
             # remove the newline characters
-            # lines = [line.strip() for line in lines]
-            # lines = [list(map(int, line.split())) for line in lines]
-            # vertices = np.array(lines)
-            # if vertices.shape[0] == dim:
-            #     vertices = vertices.T
+            lines = [line.strip() for line in lines]
+            lines = [list(map(int, line.split())) for line in lines]
+            vertices = np.array(lines)
+            if vertices.shape[0] == dim:
+                vertices = vertices.T
 
-            # if _arrays_equal_up_to_row_permutation(vertices, -vertices):
-            #     # save the vertices to a file
-            #     # list_vertices = [list(v) for v in list(vertices)]
-            #     # with open("reflexive_vertices.txt", "a") as fp:
-            #     #     fp.write(f"{list_vertices}\n")
+            p = Polytope(vertices=vertices)
+            # do something
 
-            #     p = Polytope(vertices=vertices)
-            #     h = p.h_star_vector
-            #     g = p.gamma_vector
-
-            #     gamma_positive = all(g_i >= 0 for g_i in g)
-            #     if not gamma_positive:
-            #         print(h)
-            #         print(g)
-
-            #     # print(p.h_star_vector)
-            count += 1
-        print(f"v{n_verts:02d} - {count}")
         n_verts += 1
-        tot += count
-    print(tot)
