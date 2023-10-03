7df66d18e33e8d9edc1e8abfb4f0e914d7fe90ec
Gabriele Balletti <gabriele.balletti@gmail.com>
2023-10-03 20:12:07 +0200

    Fix ks db script (#26)
    * Fix ks db script
    
    * Fix ruff warnings

--- experiments/ks_database.py
+++ experiments/ks_database.py
@@ -2,15 +2,20 @@ import numpy as np
 from sympol import Polytope
 import pathlib
 from itertools import islice
-from sympol._utils import _arrays_equal_up_to_row_permutation
 
 DATA_FOLDER = ".data/4p_reflexive"
 
 
-def _get_n_lines_iterator(filename, n):
+def _get_n_lines_iterator(filename):
     with open(filename) as fp:
         for line in fp:
-            yield [line] + list(islice(fp, n - 1))
+            try:
+                n, m = line.split()[:2]
+                n, m = int(n), int(m)
+            except:  # noqa
+                break
+            n_lines = 4 if n == 4 else n
+            yield list(islice(fp, n_lines))
 
     # None of the elements in B were found too many times, and the lists are
     # the same length, they are a permutation
@@ -32,32 +37,15 @@ if __name__ == "__main__":
             continue
         print(f"Checking v{n_verts:02d}...")
 
-        if n_verts % 2 != 0:
-            n_verts += 1
-            continue
-
-        for lines in _get_n_lines_iterator(filename, dim + 1):
-            if len(lines) < dim + 1:
-                break
+        for lines in _get_n_lines_iterator(filename):
             # remove the newline characters
             lines = [line.strip() for line in lines]
-            lines = lines[1:]
             lines = [list(map(int, line.split())) for line in lines]
-            vertices = np.array(lines).T
+            vertices = np.array(lines)
+            if vertices.shape[0] == dim:
+                vertices = vertices.T
 
-            if _arrays_equal_up_to_row_permutation(vertices, -vertices):
-                p = Polytope(vertices=vertices)
-                h = p.h_star_vector
-                g = p.gamma_vector
+            p = Polytope(vertices=vertices)
+            # do something
 
-                gamma_positive = all(g_i >= 0 for g_i in g)
-                if not gamma_positive:
-                    print(h)
-                    print(g)
-
-            # print(p.h_star_vector)
-            count += 1
-        # print(f"v{n_verts:02d} - {count}")
         n_verts += 1
-        tot += count
-    print(tot)
