853c616b055af15642b44d22c64052b744aad2f7
Gabriele Balletti <gabriele.balletti@gmail.com>
2024-03-19 22:34:00 +0100

    Run black

--- src/sympol/__init__.py
+++ src/sympol/__init__.py
@@ -4,6 +4,7 @@ TODO: Add more information about the package here.
 
 For more information, see the documentation for each class and method.
 """
+
 from sympol.point import Point  # noqa: F401
 from sympol.point_configuration import PointConfiguration  # noqa: F401
 from sympol.polytope import Polytope  # noqa: F401
